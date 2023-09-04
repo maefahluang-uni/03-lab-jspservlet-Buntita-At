7bda6b3e199e78f93c2709c9cc1e9377d36f6445
Buntita-At <122780019+Buntita-At@users.noreply.github.com>
2023-09-04 09:11:48 +0000

    6531503043

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,49 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+    double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
-
-        //TODO: forward to jsp
-           
-    }
     
+
+        double bmi = calculateBMI(weight, height);
+
+
+
+        String bodyType = determineBodyType(bmi);
+
+    
+
+        request.setAttribute("bmi", Math.round(bmi));
+        request.setAttribute("bodyType", bodyType);
+
+
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+    }
+
+    // Calculate BMI method
+    private double calculateBMI(double weight, double height) {
+        // BMI formula: BMI = weight(kg) / (height(m) * height(m))
+        return weight / (height * height);
+    }
+
+    // Determine body type method (a simple example)
+    private String determineBodyType(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "normal weight";
+        } else if (bmi >= 24.9 && bmi < 29.9) {
+            return "overweight";
+        } else {
+            return "obese";
+        }
+    }
 }
+
